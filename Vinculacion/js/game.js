c891a86ef7652e8b2129d489e5780fa5d5e3275b
FNCSuejoo <87918293+FNCSuejoo@users.noreply.github.com>
2021-08-09 18:10:14 -0500

    Add files via upload

---
+++ Vinculacion/js/game.js
@@ -0,0 +1,20 @@
+var puntaje ;
+
+
+
+
+
+    var nivel1 = document.getElementById("puntaje1");
+    var nivel2 = document.getElementById("puntaje2");
+    var nivel3 = document.getElementById("puntaje3");
+    var nivel4 = document.getElementById("puntaje4");
+    var nivel5 = document.getElementById("puntaje5");
+    var nivel6 = document.getElementById("puntaje6");
+    
+window.onload = sumar();
+    function sumar(){
+
+        //puntaje = nivel1+nivel2+nivel3+nivel4+nivel5+nivel6;
+        document.getElementById("puntajes").innerHTML = "xd"; 
+    
+    }
